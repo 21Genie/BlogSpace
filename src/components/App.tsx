fa57147447f562b6116d9037253be3e3f5c5009c
Daniel <muravtsev.danil@mail.ru>
2025-07-04 15:51:53 +0300

    добавлена вспомогательная функция classNames

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,12 +7,13 @@ import { MainPageAsync } from '../pages/MainPage/MainPage.async'
 
 import '../styles/index.scss'
 import { useTheme } from '../theme/useTheme'
+import { classNames } from '../helpers/classNames/classNames'
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme()
-
+    
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', [theme])}>
             <button onClick={toggleTheme}>ToggleTheme</button>
             <Link to={'/'}>Main Page</Link>
             <Link to={'/about'}>About Page</Link>
