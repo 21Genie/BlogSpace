bfb239dd8a8639702f2d81ad7140dc5c37f92d34
Daniel <muravtsev.danil@mail.ru>
2025-07-04 12:23:10 +0300

    добавлен react-router-dom и lazy loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import style from './App.module.scss'
+import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from '../pages/MainPage/MainPage.async'
+import { Link } from 'react-router-dom'
+import { Suspense } from 'react'
 
 export const App = () => {
     return (
         <div className={style.wrap}>
-            <div className='app'>
-                123
-            </div>
+            <Link to={'/'}>Main Page</Link>
+            <Link to={'/about'}>About Page</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPageAsync/>}/>
+                    <Route path='/about' element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     )
 }
