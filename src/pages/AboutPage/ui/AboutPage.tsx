86d315fb87af02bba7331f4fe507ccdeec6a6342
Daniel <muravtsev.danil@mail.ru>
2025-07-22 12:35:21 +0300

    исправлены скриншотные тесты

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,10 +1,11 @@
 import { useTranslation } from 'react-i18next';
+import cls from './AboutPage.module.scss';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
 
     return (
-        <div>
+        <div className={cls.about}>
             {t('О нас')}
         </div>
     );
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from 'react-i18next';
 
+import cls from './MainPage.module.scss';
+
 const MainPage = () => {
     const { t } = useTranslation('main');
 
     return (
-        <div>
+        <div className={cls.main}>
             {t('Главная')}
         </div>
     );
