8bca3d4ad4c9e79fe6edfdf82476c2a21fde6267
Daniel <muravtsev.danil@mail.ru>
2025-09-10 13:02:05 +0300

    доабавлен jest-html-reports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -42,6 +42,16 @@ export default {
     setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
 
     rootDir: '../../',
+
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
